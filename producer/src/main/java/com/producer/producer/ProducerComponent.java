6fd2564935eb4c0534802ed28c64f2848b0386bb
medXPS <mohamedammaha2020@gmail.com>
2023-09-07 14:23:29 +0100

    adding deployments

---
+++ producer/src/main/java/com/producer/producer/ProducerComponent.java
@@ -0,0 +1,26 @@
+package com.producer.producer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ProducerComponent {
+
+    private final ProducerService service;
+
+    public ProducerComponent(ProducerService service) {
+        this.service = service;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void sendToTopic() {
+        service.send("trx-events-topic");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendToDirect() {
+        service.send("trx-events-direct");
+    }
+
+}
---
+++ producer/src/main/java/com/producer/producer/SampleMessage.java
@@ -0,0 +1,14 @@
+package com.producer.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SampleMessage {
+    private Integer id;
+    private String message;
+    private String type;
+}
